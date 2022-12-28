b14e3e08d125c7cae934baeed06d40c8253f4b57
granadalook <tiven17.jsga@gmail.com>
2022-12-28 16:18:31 -0500

    Fix

--- src/invoice/invoice.module.ts
+++ src/invoice/invoice.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { InvoiceController } from './controllers/invoice/invoice.controller';
-import { Factura } from './entity/envoice.entity';
-import { InvoiceService } from './services/invoice/invoice.service';
-import { DetailInvoiceService } from './services/detail-invoice/detail-invoice.service';
 import { detail } from './entity/detailInvoice.entity';
+import { Factura } from './entity/envoice.entity';
+import { DetailInvoiceService } from './services/detail-invoice/detail-invoice.service';
+import { InvoiceService } from './services/invoice/invoice.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Factura, detail])],
